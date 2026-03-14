90fff4717a95b9f9edc1e920600dbdda3c799348
unknown <matthewkesete12@gmail.com>
2026-03-14 16:41:09 +0300

    Add SSL config for Railway production

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,13 @@
 const { Pool } = require('pg');
 
+const isProduction = process.env.DATABASE_URL ? true : false;
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL || 'postgresql://postgres:root@localhost:5432/manya_db',
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
+    ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 pool.connect((err, client, release) => {
